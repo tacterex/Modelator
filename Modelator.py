24ce82bd5861a65be29c53c44adc9e216b3e6d61
tacterex <ihor070722@gmail.com>
2021-06-08 20:43:54 +0300

    Update Modelator.py

--- Modelator.py
+++ Modelator.py
@@ -1,6 +1,15 @@
 from tkinter import Tk, Canvas, BOTH
+import math
 
 
 
+SQRT2 = math.sqrt(2)
+SQRT3 = math.sqrt(3)
 selfWindow = Tk()
 selfCanvas = Canvas(selfWindow)
+def init():
+    selfWindow.title("Welcome to modelator window!")
+    selfWindow.geometry("600x600")
+def update():
+    selfCanvas.pack(fill=BOTH, expand=1)
+    selfWindow.mainloop()
