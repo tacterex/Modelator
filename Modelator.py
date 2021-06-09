53f5d5b6e9af3a03ae9cd71d19524d0e9cf01f6b
tacterex <ihor070722@gmail.com>
2021-06-09 13:52:16 +0300

    Update Modelator.py

--- Modelator.py
+++ Modelator.py
@@ -1,5 +1,6 @@
-from tkinter import Tk, Canvas, BOTH
+from tkinter import Tk, Canvas, BOTH, ALL
 import math
+import keyboard
 
 
 
@@ -7,9 +8,21 @@ SQRT2 = math.sqrt(2)
 SQRT3 = math.sqrt(3)
 selfWindow = Tk()
 selfCanvas = Canvas(selfWindow)
+
+rotationx, rotationy, rotationz = 0, 0, 25
+
 def init():
     selfWindow.title("Welcome to modelator window!")
     selfWindow.geometry("600x600")
+def rebuild(rx, ry, rz):
+    selfCanvas.delete(ALL)
+    rx, ry, rz = rx%360, ry%360, rz%360
+    if 0 <= rz <= 90:
+        selfCanvas.create_rectangle(
+            0, 300-300*rz/90, 600, 300+300*rz/90,
+            outline="black", fill="lightgray"
+        )
 def update():
+    rebuild(rotationx, rotationy, rotationz)
     selfCanvas.pack(fill=BOTH, expand=1)
     selfWindow.mainloop()
