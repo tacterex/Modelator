4d9803c1f144a9ad8b87fdc02408eb30164a6838
tacterex <ihor070722@gmail.com>
2021-06-10 18:29:44 +0300

    Update windows.py

--- windows.py
+++ windows.py
@@ -1,4 +1,37 @@
-from tkinter import Tk, Canvas, Button, BOTH
+from tkinter import Tk, Canvas, Button, BOTH, PhotoImage, LEFT
+
+
+
+class buildWindow(Tk):
+    def __init__(self, n=1):
+        super().__init__()
+        self.n = n
+        self.selfCanvas = Canvas(self, width=600, height=600)
+        self.PIXELVIRTUAL = PhotoImage(width = 1, height = 1)
+        self.buildself()
+    def buildself(self):
+        self.geometry("600x700")
+        self.resizable(False, False)
+        self.title(f"{self.n}D window")
+        self.CubeButton = Button(self, text = "Cube",
+        image = self.PIXELVIRTUAL, width  = 300, height = 100, justify="center"
+        )
+        self.CubeButton.pack()
+
+currentWindow = {"n": None, "window": None, "canvas": None}
+def d2():
+    currentWindow["n"] = 2
+    currentWindow["window"] = buildWindow(currentWindow["n"])
+    currentWindow["canvas"] = currentWindow["window"].selfCanvas
+def d3():
+    currentWindow["n"] = 3
+    currentWindow["window"] = buildWindow(currentWindow["n"])
+    currentWindow["canvas"] = currentWindow["window"].selfCanvas
+def d4():
+    currentWindow["n"] = 4
+    currentWindow["window"] = buildWindow(currentWindow["n"])
+    currentWindow["canvas"] = currentWindow["window"].selfCanvas
+funs = [d2, d3, d4]
 
 
 
@@ -10,24 +43,13 @@ class startWindow(Tk):
     
     def buildself(self):
         self.title("start")
-        for i in range(4):
+        for i in range(3):
             obj = Button(self,
-                text=f"{i+1}D", height = 2, width = 35, justify="center",
-                command=None
+                text=f"{i+2}D", height = 2, width = 35, justify="center",
+                command=funs[i]
             )   
-            obj.pack()
+            obj.pack(side=LEFT)
             self.resizable(False, False)
-
-
-
-class buildWindow(Tk):
-    def __init__(self):
-        super().__init__()
-        selfCanvas = Canvas(self)
-
-    def buildself(self):
-        self.geometry("600x700")
-        self.cubeButton = Button(text = "Cube")
 
 
 
