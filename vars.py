2b02561e71698edaec381a865c25071d5edfd1a4
tacterex <ihor070722@gmail.com>
2021-06-10 16:30:16 +0300

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,14 +1,25 @@
-from tkinter import Tk, Canvas, BOTH, ALL
+from tkinter import Tk, Canvas, Button, BOTH
 
 
 
-selfWindow = Tk()
-selfCanvas = Canvas(selfWindow)
-selfWindow.title("Welcome to modelator window!")
-selfWindow.geometry("600x600")
-selfWindow.resizable(False, False)
-selfCanvas.focus_set()
+#
+curWindow = {"n": None, "window": None, "canvas": None}
+def openWindow(index):
+    curWindow["n"] = index+1
+    curWindow["window"] = Tk()
+    curWindow["canvas"] = Canvas(curWindow["window"])
+    curWindow["window"].geometry("600x600")
+    curWindow["window"].title(f"{index+1}D window")
+#
 
 
 
-rotationx, rotationy, rotationz = 0, 0, 0
+startWindow = Tk()
+startWindow.title("start")
+for i in range(4):
+    obj = Button(startWindow,
+        text=f"{i+1}D", height = 2, width = 35, justify="center",
+        command=lambda: openWindow(i)
+    )
+    obj.pack()
+startWindow.resizable(False, False)
