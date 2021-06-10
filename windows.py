4a6c298de53f5494cf9561b6490a3a876b883471
tacterex <ihor070722@gmail.com>
2021-06-10 20:02:36 +0300

    Update windows.py

--- windows.py
+++ windows.py
@@ -1,4 +1,6 @@
-from tkinter import Tk, Canvas, Button, BOTH, PhotoImage, LEFT
+from tkinter import Tk, Canvas, Button, BOTH
+import tkinter as tk
+import obj
 
 
 
@@ -6,17 +8,37 @@ class buildWindow(Tk):
     def __init__(self, n=1):
         super().__init__()
         self.n = n
-        self.selfCanvas = Canvas(self, width=600, height=600)
-        self.PIXELVIRTUAL = PhotoImage(width = 1, height = 1)
+        self.selfCanvas = Canvas(self, width=600, height=600, background="lightgray")
         self.buildself()
+        self.selfCanvas.pack(fill=BOTH, expand=1)
+
     def buildself(self):
         self.geometry("600x700")
         self.resizable(False, False)
         self.title(f"{self.n}D window")
-        self.CubeButton = Button(self, text = "Cube",
-        image = self.PIXELVIRTUAL, width  = 300, height = 100, justify="center"
+        self.CubeButton = Button(self, text = "Square",
+        justify="center", font="Arial 26",
+        command=lambda: obj.Square(self.selfCanvas, self.n, 300, 300, 300)
         )
-        self.CubeButton.pack()
+        self.CubeButton.place(x=0, y=600, width = 300, height = 100)
+        self.CircleButton = Button(self, text = "Circle",
+        justify="center", font="Arial 26"
+        )
+        self.CircleButton.place(x=300, y=600, width = 300, height = 100)
+        #
+        self.selfCanvas.create_line(
+            0, 300, 600, 300, arrow=tk.LAST
+        )
+        self.selfCanvas.create_line(
+            300, 600, 300, 0, arrow=tk.LAST
+        )
+        if self.n > 2:
+            self.selfCanvas.create_line(
+                0, 600, 600, 0, arrow=tk.LAST
+            )
+
+
+    
 
 currentWindow = {"n": None, "window": None, "canvas": None}
 def d2():
@@ -48,7 +70,7 @@ class startWindow(Tk):
                 text=f"{i+2}D", height = 2, width = 35, justify="center",
                 command=funs[i]
             )   
-            obj.pack(side=LEFT)
+            obj.pack()
             self.resizable(False, False)
 
 
