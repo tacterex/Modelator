3e67326e000dd651fcfdf486396a0efd86d0d4e9
tacterex <ihor070722@gmail.com>
2021-06-11 14:20:55 +0300

    Update obj.py

--- obj.py
+++ obj.py
@@ -4,16 +4,32 @@ import math
 SQRT2 = math.sqrt(2)
 SQRT3 = math.sqrt(3)
 
+AXES = [
+    [0, 300, 600, 300],
+    [300, 600, 300, 0],
+    [0, 600, 600, 0],
+    [0, 0, 600, 600]
+]
+
+polus = lambda a: a/abs(a)
+
 class Square:
     def __init__(self, canvas:Canvas, n, pointx, pointy, size):
         self.canvas = canvas
         self.n = n
         self.x, self.y = pointx, pointy
         self.a = abs(size)
+        self.verts = []
         self.buildself()
     
     def buildself(self):
         if self.n == 2:
-            self.canvas.create_rectangle(
-                self.x-self.a/2, self.y-self.a/2, self.x+self.a/2, self.y+self.a/2
-            )
+            self.verts = [self.x, self.y, self.x+self.a, self.y+self.a]
+            self.canvas.create_rectangle(self.verts)
+        else:
+            temp = polus((AXES[self.n][3] - AXES[self.n][1]) / (AXES[self.n][0] - AXES[self.n][2]))
+            Square(self.canvas, self.n-1, self.x, self.y, self.a) + Square(self.canvas, self.n-1, self.x+temp*SQRT2*self.a, self.y-temp*SQRT2*self.a, self.a)
+
+    def __add__(self, other):
+        self.buildself()
+        other.buildself()
