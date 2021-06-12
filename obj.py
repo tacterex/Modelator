64d895b2ca3bf3d33324d01664b4539fa57ff6ad
tacterex <ihor070722@gmail.com>
2021-06-12 17:37:01 +0300

    Update obj.py

--- obj.py
+++ obj.py
@@ -5,37 +5,37 @@ import math
 SQRT2 = math.sqrt(2)
 SQRT3 = math.sqrt(3)
 
-AXES = [
-    [5, 300, 600, 300],
-    [5, 600, 5, 0],
+SIGN = lambda a: a/abs(a) if a!= 0 else 0
 
-
-    [5, 300, 97.476, 0],
-
-    [5, 300, 217.963, 0],
-    
-    [5, 300, 412.915, 0],
-
-    [5, 300, 600, 105.048],
-    [5, 300, 600, 494.952],
-
-    [5, 300, 412.915, 600],
-
-    [5, 300, 217.963, 600],
-
-    [5, 300, 97.476, 600]
-]
-
-print(len(AXES))
-
-SIGN = lambda a: a/abs(a)
+def AXES(n):
+    arr = [
+        [0, 300, 600, 300],
+        [10, 600, 10, 0]
+    ]
+    tempAngle = 90/((n+1)//2)
+    delta = 1
+    for i in range(n-2):
+        angle = -90+tempAngle*(i+delta)
+        if angle == 0:
+            delta+=1
+            angle = -90+tempAngle*(i+delta)
+        sign = SIGN(angle)
+        angle = abs(angle)
+        t = math.tan(math.radians(angle))
+        if t >= 0.5:
+            arr.append([10, 300, 300/t, 300+sign*300])
+        else:
+            arr.append([10, 300, 600, 300+sign*300*t])
+    return arr
 
 cosFormTg = lambda a, b: abs(b)/math.sqrt(a**2 + b**2)
 sinFromTg = lambda a, b: abs(a)/math.sqrt(a**2 + b**2)
 
 
 class Square:
-    def __init__(self, canvas:Canvas, n, pointx, pointy, size, level=1):
+    def __init__(self, canvas:Canvas, n, pointx, pointy, size, level=1, color=None, axs=None):
+        self.axes = axs
+        self.color = color
         self.canvas = canvas
         self.n = n
         self.x, self.y = pointx, pointy
@@ -48,21 +48,25 @@ class Square:
         if self.n == 2:
             x0, y0, x1, y1 = self.x, self.y, self.x+self.a, self.y-self.a
             self.verts = [[x0, y0], [x0, y1], [x1, y1], [x1, y0]]
-            self.canvas.create_rectangle(self.x, self.y, self.x+self.a, self.y-self.a, width=1)
+            self.canvas.create_rectangle(self.x, self.y, self.x+self.a, self.y-self.a, width=1, outline=self.color)
         else:
-            a = AXES[self.n-1][3] - AXES[self.n-1][1]
-            b = AXES[self.n-1][0] - AXES[self.n-1][2]
+            a = self.axes[self.n-1][3] - self.axes[self.n-1][1]
+            b = self.axes[self.n-1][0] - self.axes[self.n-1][2]
             sign  = SIGN(a/b)
             sin = sinFromTg(a, b)
             cos = cosFormTg(a, b)
-            self.verts = Square(self.canvas, self.n-1, self.x, self.y, self.a, self.level-1) + Square(self.canvas, self.n-1, self.x+cos*self.a, self.y-sign*sin*self.a, self.a, self.level-1)
+            if self.level == 1:
+                self.verts = Square(self.canvas, self.n-1, self.x, self.y, self.a, self.level-1, "red", self.axes) + Square(self.canvas, self.n-1, self.x+cos*self.a, self.y-sign*sin*self.a, self.a, self.level-1, "blue", self.axes)
+            elif self.level < 1:
+                self.verts = Square(self.canvas, self.n-1, self.x, self.y, self.a, self.level-1, self.color, self.axes) + Square(self.canvas, self.n-1, self.x+cos*self.a, self.y-sign*sin*self.a, self.a, self.level-1, self.color, self.axes)
 
     def __add__(self, other):
         self.buildself()
         other.buildself()
         for vert in range(len(self.verts)):
             self.canvas.create_line(
-                self.verts[vert][0], self.verts[vert][1], other.verts[vert][0], other.verts[vert][1], arrow=tk.LAST if self.level==0 else None, width=1
+                self.verts[vert][0], self.verts[vert][1], other.verts[vert][0], other.verts[vert][1],
+                arrow=tk.LAST if self.level==0 else None, width=1, fill=self.color if self.level < 0 else None
             )
         temp = []
         for elem in self.verts:
